335ce3523b76a48e4f6be897179d50b38eefc87c
AaronHsieh <piafyoyo06@gmail.com>
2024-08-01 01:41:47 +0800

    新增 gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Hex2024-Vue-Week1-Homework/",
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
